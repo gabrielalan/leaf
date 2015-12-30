a8fbae9c4e169185e2995df464bb9c977c1e2e78
Gabriel Alan <gaba.alan@gmail.com>
2015-12-30 14:07:33 -0200

    IconMoon added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 var gulp = require('gulp');
-var watch = require('gulp-watch');
-var batch = require('gulp-batch');
 var handlebars = require('gulp-handlebars');
 var wrap = require('gulp-wrap');
 var declare = require('gulp-declare');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 var defineModule = require('gulp-define-module');
 
 gulp.task('handlebars', function () { 
@@ -14,10 +13,15 @@ gulp.task('handlebars', function () {
 		.pipe(gulp.dest('front/js/Templates/'));
 });
 
+gulp.task('sass', function () {
+	gulp.src('front/style/**/*.scss')
+		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(gulp.dest('front/build/css'));
+});
+
 gulp.task('watch', function () {
-	watch('front/templates/**/*.html', batch(function (events, done) {
-		gulp.start('handlebars', done);
-	}));
+	gulp.watch('front/templates/**/*.html', ['handlebars']);
+	gulp.watch('front/style/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['build']);
